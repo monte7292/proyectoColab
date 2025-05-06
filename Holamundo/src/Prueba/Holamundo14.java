5f47e2245fed2c14ca149c5db12d05412ffba3bb
monte7292 <171730096+monte7292@users.noreply.github.com>
2025-05-06 18:00:30 +0200

    Pruebaaa

--- Holamundo/src/Prueba/Holamundo14.java
+++ Holamundo/src/Prueba/Holamundo14.java
@@ -3,5 +3,6 @@ package Prueba;
 public class Holamundo14 {
 	public void escribirlinea() {
 		System.out.println("Hola Mundo soy Montero");
+		System.out.println("Hola Mundo soy Montero prueba 2");
 	}
 }
